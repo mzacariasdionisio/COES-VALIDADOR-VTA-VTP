178b9db62489f9283c02f7ea5083ae2cf775d9a7
Miguel Zacarias Dionisio <mzacarias@net-consultores.com.pe>
2025-08-18 19:31:43 -0500

    Actualizacion 20250818

--- COES.MVC.Intranet/Areas/ValidacionVTEAVTP/Controllers/ValidadorVTPSalidaController.cs
+++ COES.MVC.Intranet/Areas/ValidacionVTEAVTP/Controllers/ValidadorVTPSalidaController.cs
@@ -181,7 +181,7 @@ namespace COES.MVC.Intranet.Areas.ValidacionVTEAVTP.Controllers
 
                 var fecha = DateTime.Now;
                 model.StrMensaje = inicializar > 0 ? "NOTA: Dar clic en \"Procesar\" para realizar la evaluación" :
-                     string.Format("NOTA: Se realizó la evaluación el {0} a las {1}.", fecha.ToString("dd/MM/yyyy"), fecha.ToString("hh:mm:ss"));
+                     string.Format("NOTA: Se realizó la evaluación el {0} a las {1}.", fecha.ToString("dd/MM/yyyy"), fecha.ToString("HH:mm:ss"));
 
                 Session[Helper.ConstantesValidacionVTEAVTP.D_Datos_Salida_VTP] = model.VtpValidacion;
                
